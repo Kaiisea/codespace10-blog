09aca56288678985501dc83bc5312c1d165b8a13
kai <abacatemarinheiro@gmail.com>
2022-06-06 16:59:48 +0200

    Modelo de datos

---
+++ src/Repository/ESpacioRepository.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\ESpacio;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<ESpacio>
+ *
+ * @method ESpacio|null find($id, $lockMode = null, $lockVersion = null)
+ * @method ESpacio|null findOneBy(array $criteria, array $orderBy = null)
+ * @method ESpacio[]    findAll()
+ * @method ESpacio[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class ESpacioRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, ESpacio::class);
+    }
+
+    public function add(ESpacio $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(ESpacio $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+//    /**
+//     * @return ESpacio[] Returns an array of ESpacio objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('e')
+//            ->andWhere('e.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('e.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?ESpacio
+//    {
+//        return $this->createQueryBuilder('e')
+//            ->andWhere('e.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
